24ed507fc301423bdc710b4392e8e937f1afbeee
Koustav Biswas <bkoustav361@gmail.com>
2025-02-07 12:47:00 +0530

    Updated changes

--- .vscode/Javascript/test.js
+++ .vscode/Javascript/test.js
@@ -1,1 +1,11 @@
-console.log("LOL");
+const accountId=144553
+let accountemail= "Koustavbiswas04@gmail.com"
+var password= "Jaipur"
+
+//accountId=2//not allowed
+console.log(accountId)
+
+
+accountemail="k@gmail.com"
+console.log(accountemail)
+console.table([accountId,accountemail,password])
